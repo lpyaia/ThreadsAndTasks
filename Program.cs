e24d033017c84c3cb834c6a1aba3d1e62836c2c4
Lucas Pyaia <lucaspyaia@hotmail.com>
2022-10-08 20:15:35 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿var t1 = new Thread(() =>
+{
+    int count = 0;
+
+    while (count < 60)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine($"Seconds: {++count}");
+    }
+});
+
+t1.Start();
+
+Console.WriteLine("End");
